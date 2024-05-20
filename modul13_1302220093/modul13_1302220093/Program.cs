37a6647df18435b0fdcc733669206e79ee105ecf
Rd. M. Faisal Ramadhan J <151460358+RdMFaiXall@users.noreply.github.com>
2024-05-20 08:00:43 +0700

    menyelesaikan praktikum minggu 13 kpl

---
+++ modul13_1302220093/modul13_1302220093/Program.cs
@@ -0,0 +1,41 @@
+﻿using modul13_1302220093;
+using System;
+
+namespace ProgramUtama
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        { 
+            // deklarasi variabel data1 dan data2 bertipe data PusatDataSingleton
+            PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+            PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+            // menambahkan semua nama anggota kelompok dan asprak pada list
+            data1.AddSebuahData("Rd. M. Faisal Ramadhan Junaidi");
+            data1.AddSebuahData("Cokorda Arturito Revan Putra Diarta");
+            data1.AddSebuahData("Faishal Harits Al-Fajri");
+            data1.AddSebuahData("Rifqi Alghifari");
+            data1.AddSebuahData("Farrel Haykal Giffari");
+            data1.AddSebuahData("Riva Dwi Fadhillah Fajri Permana");
+            data1.AddSebuahData("NCA");
+
+            // menampilkan data pada classs PusatDataSingleton
+            data2.PrintSemuaData();
+            Console.WriteLine();
+
+            // menghapus data asprak pada index 6 list
+            data2.HapusSebuahData(6);
+
+            // menampilkan data lagi pada class PusatDataSingleton
+            data1.PrintSemuaData();
+            Console.WriteLine();
+
+            // menampilkan jumlah data pada data1 dan data2
+            Console.WriteLine(data1.GetSemuaData().Count());
+            Console.WriteLine(data2.GetSemuaData().Count()); 
+
+
+        }   
+    }
+}
---
+++ modul13_1302220093/modul13_1302220093/PusatDataSingleton.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul13_1302220093
+{
+    // Deklarasi class bernama PusatDataSingleton
+    public class PusatDataSingleton
+    {
+        // deklarasi variabel
+        private List<String> DataTersimpan;
+        private static PusatDataSingleton _instance;
+
+        // deklarasi constructor untuk DataTersimpan
+        private PusatDataSingleton()
+        {
+            DataTersimpan = new List<String>();
+        }
+
+        // deklarasi method PusatDataSingleton untuk mengambil data Class PusatDataSingletin
+        public static PusatDataSingleton GetDataSingleton() { 
+            if (_instance == null)
+            {
+                _instance = new PusatDataSingleton();
+            }
+            return _instance;
+        }
+
+
+        // deklarasi method yang mengembalikan tipe List String untuk mengambil semua Data variabel DataTersimpan
+        public List<String> GetSemuaData()
+        {
+            return DataTersimpan;
+        }
+
+        // deklarasi method PrintSemuaData untuk menampilkan semua DataTersimpan pada console
+        public void PrintSemuaData()
+        {
+            for (int i = 0; i < DataTersimpan.Count; i++)
+            {
+                Console.WriteLine(DataTersimpan[i]);
+            }
+        }
+
+        // deklarasi method AddSebuahData untuk menambahkan data baru pada list DataTersimpan
+        public void AddSebuahData(String Input)
+        {
+            DataTersimpan.Add(Input);
+        }
+
+        // deklarasi method HapusSebuahData untuk menghapus data berdasarkan lokasi index
+        public void HapusSebuahData(int index)
+        {
+            DataTersimpan.RemoveAt(index);
+        }
+
+    }
+}
